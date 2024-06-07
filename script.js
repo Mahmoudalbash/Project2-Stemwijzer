dfc6b5660309aa996e08236459b6c1bfdcc42480
DJSUMMA <ps226543@summacollege.nl>
2024-06-07 10:00:40 +0200

    new file:   Account.html 	new file:   Nieuws.html 	new file:   Opties.html 	new file:   Over_de_partijen.html 	new file:   Over_ons.html 	new file:   Start.html 	new file:   Stellingen.html 	new file:   images/logo-met-text-rechts.svg 	new file:   images/logo-neutraal-kieslab-donkerblauw.svg 	new file:   images/logo-neutraal-kieslab-lichtblauw.svg 	new file:   images/logo-tegels-achtergrond.svg 	new file:   script.js 	new file:   styles.css

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleCheckbox = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            themeToggleCheckbox.checked = true;
+        }
+    }
+
+    themeToggleCheckbox.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
+        
+        let theme = 'light-mode';
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
